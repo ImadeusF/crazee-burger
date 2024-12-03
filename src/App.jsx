55d64da79d54c30ae0dbaad7c0b0f88b7b48430a
imadeusF <damien.fournier1981@gmail.com>
2024-12-03 11:22:42 +0100

    ✨ F01: LOGIN > Add title and form - delete .svg

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from "react";
+
 import LoginPage from "./components/pages/LoginPage.jsx";
 
 function App() {
-  const [prenom, setPrenom] = useState("");
-
-  const handleSubmit = (e) => {
-    if (!e.target.closest("form").checkValidity()) {
-      return; // Ne bloquez pas le comportement natif du navigateur
-    } else {
-      e.preventDefault();
-      alert(`Bonjour ${prenom}!`);
-      setPrenom("");
-    }
-  };
 
   return (
     <div>
       <LoginPage />
-      <h1>Bienvenue chez nous!</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={prenom}
-          placeholder="Entrez votre prénom"
-          required
-          onChange={(e) => setPrenom(e.target.value)}
-        />
-        <button type="submit">Accéder à votre espace</button>
-      </form>
+
     </div>
   );
 }
--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+
 function LoginPage() {
+  const [inputvalue, setInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setInputValue(inputvalue);
+    alert(`Bonjour ${inputvalue}!`);
+    setInputValue("");
+  };
+
   return (
     <div>
-      loginPage
+      <h1>Bienvenue chez nous!</h1>
+      <br />
+      <h2>Connectez vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={inputvalue}
+          placeholder="Entrez votre prénom"
+          required
+          onChange={handleChange}
+        />
+        <button>Accéder à votre espace</button>
+      </form>
     </div>
   );
 }
