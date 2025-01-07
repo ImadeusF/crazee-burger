0f265a16426d898eaf267bfcd9fb7c42cf4b15da
Imadeus <imadeus1981@gmail.com>
2025-01-07 11:00:23 +0100

    🚧 F04: ORDER > Layout > add container

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,16 +1,33 @@
-import React from "react";
 import { Link, useParams } from "react-router";
+import { theme } from "../../../themes";
+import styled from "styled-components";
 
 export default function OrderPage() {
-  const {username} = useParams(); //paramètres de l'url
+  const { username } = useParams(); //paramètres de l'url
   // useParams renvoi un objet, on peut déstructurer pour récupérer la valeur de la clé inputValue
 
   return (
-    <div>
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
-    </div>
+    <OrderPageStyle>
+      <div className="container">
+        <h1>Bonjour {username}</h1>
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
+      </div>
+    </OrderPageStyle>
   );
 }
+
+const OrderPageStyle = styled.div`
+  background: ${theme.colors.primary};
+  height: 100vh;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    background: ${theme.colors.white};
+    height: 95vh;
+    width:1400px;F
+  }
+`;
