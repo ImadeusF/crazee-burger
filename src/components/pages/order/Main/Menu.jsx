48409c30923c81218f50f61bd2cce6ca22c9cabb
Imadeus <imadeus1981@gmail.com>
2025-01-09 11:05:29 +0100

    🚧 F05: ORDER > MENU > Add product infos

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,33 +1,54 @@
 import { useState } from "react";
-import styled from "styled-components"
+import styled from "styled-components";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../themes";
 
 export default function Menu() {
-    const [menu, setMenu] = useState(fakeMenu2);
-  
-    return (
+  const [menu, setMenu] = useState(fakeMenu2);
+
+  return (
     <MenuStyled>
       {menu.map((produit) => {
-       return <div className="produit">{produit.title}</div>
-    })}
+        return (
+          <div className="produit" key={produit.id}>
+            <div className="image">
+              <img src={produit.imageSource} alt={produit.title} />
+            </div>
+            <div className="info-texte" >
+              <div className="title">{produit.title}</div>
+              <div className="description">
+                <div className="price">{produit.price}</div>
+                <button className="add-button">Ajouter</button>
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </MenuStyled>
-  )
+  );
 }
 
 const MenuStyled = styled.div`
-    background: ${theme.colors.background_white};
-    display:grid;
-    grid-template-columns:repeat(4,1fr);
-    grid-row-gap:60px;
-    padding:50px 50px 150px;
-    justify-items:center;
-    box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  background: ${theme.colors.background_white};
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-row-gap: 60px;
+  padding: 50px 50px 150px;
+  justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
-    .produit {
-        background: red;
-        width:240px;
-        height:330px;
+  .produit {
+    background: red;
+    width: 240px;
+    height: 330px;
 
+    .image {
+      width: 200px;
+      height: 245px;
+      img {
+        width: 100px;
+        height: auto;
+      }
     }
-`
+  }
+`;
