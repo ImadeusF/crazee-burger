cd7e98c1e22fa824029fe0913f98508e55eeb70c
Imadeus <imadeus1981@gmail.com>
2025-01-09 10:53:31 +0100

    💄 F05: ORDER > MENU > Add final style

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -15,7 +15,7 @@ const MainStyled = styled.div`
   background: ${theme.colors.background_white};
   flex: 1;
 
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
 
