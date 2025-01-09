5debc89634fee561c845562718693f9d5335ddfa
Imadeus <imadeus1981@gmail.com>
2025-01-09 09:30:04 +0100

    🚧 F05: ORDER > MAIN > Add image and grid layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -1,17 +1,34 @@
 import styled from "styled-components";
 import { theme } from "../../../themes";
+import { fakeMenu2 } from "../../../fakeData/fakeMenu";
+import PrimaryButton from "../../reusable-ui/PrimaryButton";
 
 export default function Main() {
-  return <MainStyled>
-
-  </MainStyled>;
+  return (
+    <MainStyled className="main">
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
   background: ${theme.colors.background_white};
   flex: 1;
 
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset ;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+  display: grid;
+  grid-template-columns: 25% 1fr;
+  grid-template-rows: auto;
+
+  .basket {
+    background: pink;
+  } 
+
+  .menu {
+    background: purple;
+  }
 `;
