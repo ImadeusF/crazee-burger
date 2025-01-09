61203a6f87e5d77b6fb41ce84ec0e08866a28ca6
Imadeus <imadeus1981@gmail.com>
2025-01-09 12:23:10 +0100

    05: ORDER > Product > Add image layout

--- src/components/pages/order/Main/Product.jsx
+++ src/components/pages/order/Main/Product.jsx
@@ -9,8 +9,8 @@ export default function Product({ title, imageSource, price }) {
           <img src={imageSource} alt={title} />
         </div>
         <div className="info-texte">
+          <div className="title">{title}</div>
           <div className="description">
-            <div className="title">{title}</div>
             <div className="price">{price}</div>
             <button className="add-button">Ajouter</button>
           </div>
@@ -21,24 +21,30 @@ export default function Product({ title, imageSource, price }) {
 }
 
 const ProductStyled = styled.div`
- .produit {
+  .produit {
     background: ${theme.colors.white};
-    width: 240px;
+    width: 200px;
     height: 330px;
-
+    display: grid;
+    grid-template-rows: 65% 1fr;
+    padding: 20px;
+    padding-bottom: 10px;
 
     .image {
       border: 1px solid black;
+      width: 100%;
+      height: auto;
+      margin-top: 30px;
 
-      
       img {
-        width: 100px;
-        height: auto;object-fit: contain;
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
       }
     }
+  }
 
-    .description {
-      border: 1px solid black;
-    }
+  .description {
+    border: 1px solid blue;
   }
 `;
