0b65060f7fc147990a5c9e2f0a48d1fddda0d5b4
Imadeus <imadeus1981@gmail.com>
2025-01-10 12:16:12 +0100

    ♻️ F06: ODER > NAVBAR > create ToastAdmin

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,11 +1,11 @@
 import styled from "styled-components";
 import Profile from "./Profile";
 import "react-toastify/dist/ReactToastify.css";
-import { ToastContainer, toast } from "react-toastify";
-import { theme } from "../../../../themes";
+import { toast } from "react-toastify";
 import ToggleButton from "../../../reusable-ui/ToggleButton";
 import { FaUserSecret } from "react-icons/fa";
 import { useState } from "react";
+import ToastAdmin from "./ToastAdmin";
 
 export default function NavbarRightSide({ username }) {
   const [isModeAdmin, setIsModeAdmin] = useState(false);
@@ -35,7 +35,7 @@ export default function NavbarRightSide({ username }) {
         // couleurDuBackground={"green"}
         // couleurDuTexte={"white"}
       />
-      <ToastContainer className="toaster" bodyClassName="body-toast" />
+      <ToastAdmin />
       <Profile username={username} />
     </NavbarRightSideStyled>
   );
@@ -45,22 +45,4 @@ const NavbarRightSideStyled = styled.div`
   display: flex;
   align-items: center;
   padding-right: 50px;
-
-  .toaster {
-    max-width: 300px;
-  }
-
-  .Toastify__toast.Toastify__toast-theme--dark.Toastify__toast--info {
-    background: ${theme.colors.background_dark};
-  }
-
-  .body-toast {
-    .Toastify__toast-icon.Toastify--animate-icon.Toastify__zoom-enter {
-      margin-right: 20px;
-      margin-left: 5px;
-    }
-    div {
-      line-height: 1.3em;
-    }
-  }
 `;
