6e8144a65d2688cae17df2907a6f52e70c36edf8
Imadeus <imadeus1981@gmail.com>
2025-01-13 10:06:22 +0100

    💄 F07: MAIN > add layout with Basket

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -6,14 +6,18 @@ export default function Main() {
   return (
     <MainStyled className="main">
       {/* <div className="basket">Basket</div> */}
-      <Menu />
+      <div className="menu-and-admin">
+        <Menu />
+        <div className="admin">Admin</div>
+      </div>
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
+  border: 3px solid green;
   background: ${theme.colors.background_white};
-  flex: 1;
+  height: calc(95vh - 10vh);
 
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
@@ -21,10 +25,25 @@ const MainStyled = styled.div`
   display: grid;
   grid-template-columns: 1fr;
 
-  overflow-y: scroll;
-
   /* .basket {
     background: pink;
-  }  */
+  } */
 
+  .menu-and-admin {
+    position: relative;
+    display: grid;
+    overflow-y: hidden;
+    border-bottom-left-radius: ${theme.borderRadius.extraRound};
+    border-bottom-right-radius: ${theme.borderRadius.extraRound};
+
+    .admin {
+      position: absolute;
+      background: red;
+      height: 250px;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      
+    }
+  }
 `;
--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -4,22 +4,20 @@ import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 import { theme } from "../../../../themes";
 import Card from "../../../reusable-ui/Card";
 import { formatPrice } from "../../../../utils/maths";
-import AdminContext from "../../../../context/AdminContext";
 
 export default function Menu() {
   const [menu, setMenu] = useState(fakeMenu2);
-  const [isModeAdmin, setIsModeAdmin] = useState(false);
 
   return (
     <MenuStyled>
-      {menu.map(({id, title, imageSource, price}) => {
+      {menu.map(({ id, title, imageSource, price }) => {
         return (
-           <Card
-             key={id}
-             title={title}
-             imageSource={imageSource}
-             leftDescription={formatPrice(price)}
-           />  
+          <Card
+            key={id}
+            title={title}
+            imageSource={imageSource}
+            leftDescription={formatPrice(price)}
+          />
         );
       })}
     </MenuStyled>
@@ -27,11 +25,14 @@ export default function Menu() {
 }
 
 const MenuStyled = styled.div`
+  border: 1px solid blue;
   background: ${theme.colors.background_white};
   display: grid;
   grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-row-gap: 60px;
   padding: 50px 50px 150px;
   justify-items: center;
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  overflow-y: scroll;
 `;
