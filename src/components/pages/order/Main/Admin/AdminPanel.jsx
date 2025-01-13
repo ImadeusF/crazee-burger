dc2d0c385baad059ede8ee895acd6b2d23028bae
Imadeus <imadeus1981@gmail.com>
2025-01-13 11:25:57 +0100

    💄 F07: ADMIN > Add AdminTabs and AdminPanel

---
+++ src/components/pages/order/Main/Admin/AdminPanel.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import { theme } from "../../../../../themes";
+
+export default function AdminPanel() {
+  return <AdminPanelStyled>Panel</AdminPanelStyled>;
+}
+
+const AdminPanelStyled = styled.div`
+  height: 250px;
+  background: ${theme.colors.white};
+  border: 1px solid ${theme.colors.greyLight};
+  box-shadow: ${theme.shadows.subtle};
+`;
---
+++ src/components/pages/order/Main/Admin/AdminTabs.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default function AdminTabs() {
+  return <AdminTabsStyled>Tab</AdminTabsStyled>;
+}
+
+const AdminTabsStyled = styled.div`
+  border: 1px solid red;
+`;
