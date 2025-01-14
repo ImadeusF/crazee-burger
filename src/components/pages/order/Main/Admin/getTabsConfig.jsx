b84f1e785c29100aea3d393ace8f4a5c1cbf1be8
Imadeus <imadeus1981@gmail.com>
2025-01-14 13:47:57 +0100

    🔥 F07: ADMIN > Remove useless code for test

--- src/components/pages/order/Main/Admin/getTabsConfig.jsx
+++ src/components/pages/order/Main/Admin/getTabsConfig.jsx
@@ -1,25 +1,19 @@
 import { AiOutlinePlus } from "react-icons/ai";
 import { MdModeEditOutline } from "react-icons/md";
 
-export const getTabsConfig = (currentTabSelected) => [
-  // {
-  //   index: "chevronUpDown",
-  //   label: "",
-  //   Icon: isCollapsed ? <FiChevronUp /> : <FiChevronDown />,
-  //   onClick: () => setIsCollapsed(!isCollapsed),
-  //   className: isCollapsed ? "is-active" : "null",
-  // },
-  {
-    index: "add",
-    label: "Ajouter un produit",
-    Icon: <AiOutlinePlus />,
-  },
-  {
-    index: "edit",
-    label: "Modifier un produit",
-    Icon: <MdModeEditOutline />,
-  },
-];
+export const getTabsConfig = () => [
+    {
+      index: "add",
+      label: "Ajouter un produit",
+      Icon: <AiOutlinePlus />,
 
-export const getTabSelected = (tabs, currentTabSelected) =>
-  tabs.find((tab) => tab.index === currentTabSelected);
+    },
+    {
+      index: "edit",
+      label: "Modifier un produit",
+      Icon: <MdModeEditOutline />,
+    },
+  ];
+
+  export const getTabSelected = (tabs, currentTabSelected) => 
+     tabs.find((tab) => tab.index === currentTabSelected) 
