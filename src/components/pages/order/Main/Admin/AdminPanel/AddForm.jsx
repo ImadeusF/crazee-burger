e1364022ddffe9b5821f1302e5c2053a96dbf1ee
Imadeus <imadeus1981@gmail.com>
2025-01-17 10:48:47 +0100

    💄 F08: AddForm > Add submit message style

--- src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
+++ src/components/pages/order/Main/Admin/AdminPanel/AddForm.jsx
@@ -92,8 +92,8 @@ export default function AddForm() {
          />
         {isSubmited && (
           <div className="submit-message">
-            <FiCheck />
-            <span>Ajouté avec succès</span>
+            <FiCheck className="icon"/>
+            <span className="message">Ajouté avec succès !</span>
           </div>
         )}
       </div>
@@ -155,14 +155,33 @@ const AddFormStyled = styled.form`
     grid-area: submit-button;
     display: flex;
     align-items: center;
+    position:relative;
+    top:3px;
 
     .submit-button {
       width: 50%;
     }
 
     .submit-message {
-      width: 50%;
-      padding-left: 20px;
+      display:flex;
+      justify-content: center;
+      align-items: center;
+      margin-left: 5px;
+
+      .icon {
+        color: ${theme.colors.success};
+        margin-left:10px;
+        width:1em;
+        height:1em;
+        border: 1px solid ${theme.colors.success};
+        border-radius: 50%;
+      }
+
+      .message {
+        margin-left:5px;
+        font-size:${theme.fonts.size.SM};
+        color: ${theme.colors.success};
+      }
     }
   }
 `;
