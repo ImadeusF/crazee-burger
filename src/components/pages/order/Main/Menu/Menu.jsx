edadc40b516973062dfc93abf0d53abd3db2e033
Imadeus <imadeus1981@gmail.com>
2025-01-21 09:50:12 +0100

    ♻️ F09: MENU > rename productSelected with productClickedOn

--- src/components/pages/order/Main/Menu/Menu.jsx
+++ src/components/pages/order/Main/Menu/Menu.jsx
@@ -13,15 +13,17 @@ export default function Menu() {
   const { menu, isModeAdmin, handleDelete, resetMenu, setProductSelected } =
     useContext(OrderContext);
 
+  const handleClick = (idProductSelected) => {
+    const productClickedOn = menu.find(
+      (product) => product.id === idProductSelected
+    );
+    setProductSelected(productClickedOn);
+  };
+  
   if (menu.length === 0) {
     if (!isModeAdmin) return <EmptyMenuClient />;
     return <EmptyMenuAdmin onReset={resetMenu} />;
   }
-
-const handleClick = (idProductSelected) => {
-  const productSelected = menu.find((product) => product.id === idProductSelected);
-  setProductSelected(productSelected);
-}
 
   return (
     <MenuStyled>
