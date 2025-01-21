b32f81f9267d8ae79fbdd0035ff13122d01354d6
Imadeus <imadeus1981@gmail.com>
2025-01-21 10:25:38 +0100

    💄 F09: CARD > Make hoverable based on isHoverable props

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -39,8 +39,6 @@ export default function OrderPage() {
     setMenu(fakeMenu.MEDIUM);
   }
 
-
-
   const orderContextValue = {
     isModeAdmin,
     setIsModeAdmin,
--- src/components/reusable-ui/Card.jsx
+++ src/components/reusable-ui/Card.jsx
@@ -1,26 +1,39 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { theme } from "../../themes";
 import Button from "./Button";
 import { TiDelete } from "react-icons/ti";
 
-export default function Card({ title, imageSource, leftDescription, hasDeleteButton, onDelete, onClick }) {
-
-return (
-    <CardStyled className="produit" onClick={onClick}>
-      {hasDeleteButton && (
-        <button className="delete-button" aria-label="delete-button" onClick={onDelete}>
-          <TiDelete className="icon" />
-        </button>
-      )}
-      <div className="image">
-        <img src={imageSource} alt={title} />
-      </div>
-      <div className="text-info">
-        <div className="title">{title}</div>
-        <div className="description">
-          <div className="left-description">{leftDescription}</div>
-          <div className="right-description">
-            <Button className="primary-button" label={"Ajouter"} />
+export default function Card({
+  title,
+  imageSource,
+  leftDescription,
+  hasDeleteButton,
+  onDelete,
+  onClick,
+  isHoverable,
+}) {
+  return (
+    <CardStyled className="produit" onClick={onClick} isHoverable={isHoverable}>
+      <div className="card">
+        {hasDeleteButton && (
+          <button
+            className="delete-button"
+            aria-label="delete-button"
+            onClick={onDelete}
+          >
+            <TiDelete className="icon" />
+          </button>
+        )}
+        <div className="image">
+          <img src={imageSource} alt={title} />
+        </div>
+        <div className="text-info">
+          <div className="title">{title}</div>
+          <div className="description">
+            <div className="left-description">{leftDescription}</div>
+            <div className="right-description">
+              <Button className="primary-button" label={"Ajouter"} />
+            </div>
           </div>
         </div>
       </div>
@@ -29,106 +42,120 @@ return (
 }
 
 const CardStyled = styled.div`
-  position: relative;
-  background: ${theme.colors.white};
-  box-sizing: border-box;
-  width: 240px;
-  height: 330px;
-  display: grid;
-  grid-template-rows: 65% 1fr;
-  padding: 20px;
-  padding-bottom: 10px;
-  box-shadow: ${theme.shadows.medium};
-  border-radius: ${theme.borderRadius.extraRound};
+  ${(props) => props.isHoverable && hoverableStyled}
 
-  .delete-button {
-    position: absolute;
-    top: 15px;
-    right: 15px;
-    cursor: pointer;
-    width: 30px;
-    height: 30px;
-    color: ${theme.colors.primary};
-    z-index: 2;
-    padding: 0;
-    border: none;
-    background: none;
-
-    .icon {
-      width: 100%;
-      height: 100%;
-    }
-
-    :hover {
-      color: ${theme.colors.red};
-    }
-
-    :active {
-      color: ${theme.colors.primary};
-    }
-  }
-
-  .image {
-    width: 100%;
-    height: auto;
-    margin-top: 30px;
-    margin-bottom: 20px;
-
-    img {
-      width: 100%;
-      height: 100%;
-      object-fit: contain;
-    }
-  }
-
-  .text-info {
+  .card {
+    position: relative;
+    background: ${theme.colors.white};
+    box-sizing: border-box;
+    width: 240px;
+    height: 330px;
     display: grid;
-    grid-template-rows: 30% 70%;
-    padding: 5px;
+    grid-template-rows: 65% 1fr;
+    padding: 20px;
+    padding-bottom: 10px;
+    box-shadow: ${theme.shadows.medium};
+    border-radius: ${theme.borderRadius.extraRound};
 
-    .title {
-      margin: auto 0;
-      font-size: ${theme.fonts.size.P4};
-      position: relative;
-      bottom: 10px;
-      font-weight: ${theme.fonts.weights.bold};
-      color: ${theme.colors.dark};
-      text-align: left;
-      white-space: nowrap;
-      overflow: hidden;
-      width: 100%;
-      text-overflow: ellipsis;
-      font-family: "Amatic SC", cursive;
-    }
+    .delete-button {
+      position: absolute;
+      top: 15px;
+      right: 15px;
+      cursor: pointer;
+      width: 30px;
+      height: 30px;
+      color: ${theme.colors.primary};
+      z-index: 2;
+      padding: 0;
+      border: none;
+      background: none;
 
-    .description {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-
-      .left-description {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        font-weight: ${theme.fonts.weights.medium};
-        white-space: nowrap;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        font-weight: ${theme.fonts.weights.medium};
-        color: ${theme.colors.primary};
+      .icon {
+        width: 100%;
+        height: 100%;
       }
 
-      .right-description {
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        font-size: ${theme.fonts.size.P1};
+      :hover {
+        color: ${theme.colors.red};
+      }
 
-        .primary-button {
-          font-size: ${theme.fonts.size.XS};
-          cursor: pointer;
-          padding: 12px;
+      :active {
+        color: ${theme.colors.primary};
+      }
+    }
+
+    .image {
+      width: 100%;
+      height: auto;
+      margin-top: 30px;
+      margin-bottom: 20px;
+
+      img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+      }
+    }
+
+    .text-info {
+      display: grid;
+      grid-template-rows: 30% 70%;
+      padding: 5px;
+
+      .title {
+        margin: auto 0;
+        font-size: ${theme.fonts.size.P4};
+        position: relative;
+        bottom: 10px;
+        font-weight: ${theme.fonts.weights.bold};
+        color: ${theme.colors.dark};
+        text-align: left;
+        white-space: nowrap;
+        overflow: hidden;
+        width: 100%;
+        text-overflow: ellipsis;
+        font-family: "Amatic SC", cursive;
+      }
+
+      .description {
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+
+        .left-description {
+          display: flex;
+          justify-content: flex-start;
+          align-items: center;
+          font-weight: ${theme.fonts.weights.medium};
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          font-weight: ${theme.fonts.weights.medium};
+          color: ${theme.colors.primary};
+        }
+
+        .right-description {
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          font-size: ${theme.fonts.size.P1};
+
+          .primary-button {
+            font-size: ${theme.fonts.size.XS};
+            cursor: pointer;
+            padding: 12px;
+          }
         }
       }
     }
+  }
+`;
+
+const hoverableStyled = css`
+  &:hover {
+    transform: scale(1.05);
+    transition: ease-out 0.4s;
+    box-shadow: ${theme.shadows.orangeHighlight};
+    cursor: pointer;
+    border-radius: ${theme.borderRadius.extraRound};
   }
 `;
