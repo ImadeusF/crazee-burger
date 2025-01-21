a51c8744cc954119fade54d7f9899ce82eb72086
Imadeus <imadeus1981@gmail.com>
2025-01-21 10:06:42 +0100

    ♻️ F09: CARD > Add box-sizing : border-box

--- src/components/reusable-ui/Card.jsx
+++ src/components/reusable-ui/Card.jsx
@@ -31,8 +31,9 @@ return (
 const CardStyled = styled.div`
   position: relative;
   background: ${theme.colors.white};
-  width: 200px;
-  height: 300px;
+  box-sizing: border-box;
+  width: 240px;
+  height: 330px;
   display: grid;
   grid-template-rows: 65% 1fr;
   padding: 20px;
