336f48a8590ad99cfde1d7534622e9f0b070d214
Imadeus <imadeus1981@gmail.com>
2025-01-24 14:11:00 +0100

    💄 F11: BASKET > Add BasketCard (Read only from fakeBasket)

--- src/components/pages/order/Main/Basket/BasketProducts.jsx
+++ src/components/pages/order/Main/Basket/BasketProducts.jsx
@@ -1,13 +1,34 @@
 import styled from "styled-components";
+import BasketCard from "./BasketCard";
+import { formatPrice } from "../../../../../utils/maths";
 
-export default function BasketProducts() {
+export default function BasketProducts({ basket }) {
   return (
     <BasketProductsStyled>
-      <span>Liste des produits</span>
+      {basket.map((basketProduct) => (
+       <div className="basket-card" key={basketProduct.id}>
+         <BasketCard {...basketProduct} />
+       </div>
+      ))}
     </BasketProductsStyled>
   );
 }
 
 const BasketProductsStyled = styled.div`
- background-color: red;
+  flex: 1;
+  flex-direction: column;
+  overflow-y: scroll;
+
+  .basket-card {
+    margin: 10px 16px;
+    height: 86px;
+    box-sizing: border-box;
+
+    &:first-child {
+      margin-top: 20px;
+    }
+    &:last-child {
+      margin-bottom: 20px;
+    }
+  }
 `;
