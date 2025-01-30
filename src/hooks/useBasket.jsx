a4b33afcbf095be6637016e23e9b9dfe968efaa8
Imadeus <imadeus1981@gmail.com>
2025-01-30 10:49:06 +0100

    ✨ F11: BASKET > Add Product > Create handleAddToBasket (when product is not in basket)

--- src/hooks/useBasket.jsx
+++ src/hooks/useBasket.jsx
@@ -1,15 +1,32 @@
 import { useState } from "react";
 import { fakeBasket } from "../fakeData/fakeBasket";
+import { deepClone, find } from "../utils/array";
 
 export const useBasket = () => {
-  const [basket, setBasket] = useState(fakeBasket.EMPTY);
+  const [basket, setBasket] = useState(fakeBasket.SMALL);
 
-const handleAddToBasket = (productToAdd) => {
- 
-}
+  const handleAddToBasket = (productToAdd) => {
+    const basketCopy = deepClone(basket);
 
+    const isProductAlreadyInBasket =
+      find(productToAdd.id, basketCopy) !== undefined; //undefined permet de renvoyer true ou false
+    console.log("isProductAlreadyInBasket", isProductAlreadyInBasket);
+
+    if (!isProductAlreadyInBasket) {
+      const newBastketProduct = {
+        ...productToAdd,
+        quantity: 1,
+      };
+      const basketUpdated = [...basketCopy, newBastketProduct];
+
+      setBasket(basketUpdated);
+    }
+    
+
+  };
 
   return {
-    basket, handleAddToBasket
+    basket,
+    handleAddToBasket,
   };
 };
