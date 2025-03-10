60d6c5d1a72c74cbbcd351a59e3d6ae74975cc25
Imadeus <imadeus1981@gmail.com>
2025-03-10 12:47:32 +0100

    💄 THEMECOLORS > Layout adaptations

--- src/components/reusable-ui/Tab.jsx
+++ src/components/reusable-ui/Tab.jsx
@@ -1,60 +1,64 @@
 import styled from "styled-components";
-import {theme} from "../../themes";
+import { theme } from "../../themes";
 import { devices } from "../../enums/devices";
 
-
-export default function Tab({label, Icon, onClick, className}) {
+export default function Tab({ label, Icon, onClick, className }) {
   return (
     <TabStyled label={label} onClick={onClick} className={className}>
       <div className="icon">{Icon}</div>
       {label && <span className="label">{label}</span>}
     </TabStyled>
-  )
+  );
 }
 
 const TabStyled = styled.button`
-    height: 43px;
-    padding:0px 22px;
+  height: 43px;
+  padding: 0px 22px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  cursor: pointer;
+
+  position: relative;
+  left: 5%;
+  top: 1px;
+
+  //fonts
+  font-size: ${theme.fonts.size.P0};
+  color: ${theme.colors.greySemiDark};
+
+  background: ${theme.colors.white};
+  box-shadow: ${theme.shadows.subtle};
+
+  //border
+  border-width: 1px 1px 2px 1px;
+  border-style: solid;
+  border-color: ${theme.colors.greyLight};
+
+  //border-radius
+  border-radius: ${theme.borderRadius.round};
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+
+  hover {
+    border-bottom: 2px solid ${theme.colors.white};
+  }
+
+  .icon {
     display: flex;
-    justify-content: center;
-    align-items: center;
+  }
 
-    cursor: pointer;
+  .label {
+    margin-left: 13px;
+  }
 
-    position: relative;
-    left: 5%;
-    top: 1px;
-
-    //fonts
-    font-size: ${theme.fonts.size.P0};
-    color: ${theme.colors.greySemiDark};
-
-    background: ${theme.colors.white};
-    box-shadow: ${theme.shadows.subtle};
-
-    //border
-    border-width: 1px 1px 2px 1px;
-    border-style: solid;
-    border-color: ${theme.colors.greyLight};
-
-    //border-radius
-    border-radius: ${theme.borderRadius.round};
-    border-bottom-right-radius: 0;
-    border-bottom-left-radius: 0;
-
-    hover {
-        border-bottom: 2px solid ${theme.colors.white};
-    }
-
-    .icon {
-        display:flex;
-    }
-
+  @media (max-width: 550px) {
     .label {
-      margin-left:13px;
+      display: none;
     }
-
-     @media ${devices.md} {
-      padding:0px 10px;
-      }
+  }
+  @media ${devices.md} {
+    padding: 0px 10px;
+  }
 `;
